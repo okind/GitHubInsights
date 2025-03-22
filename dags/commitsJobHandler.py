7295c78e1c9c2fe3b5a066ef1b89716ff9c55d53
Olga Kindeeva <olgakindeeva@gmail.com>
2025-03-21 19:26:38 -0700

    Added commits additions, deletions, total statistics

--- dags/commitsJobHandler.py
+++ dags/commitsJobHandler.py
@@ -20,5 +20,11 @@ token = os.getenv('GITHUB_TOKEN')
 # Provide repo data: repo_owner, repo_name, token
 commits_data = gitHubClient.fetch_github_commits(REPO_OWNER, REPO_NAME, token)
 
+for commit in commits_data:
+    sha = commit['sha']
+    # Add commit stats to commit data
+    commit.update(
+        gitHubClient.fetch_gitHub_commit_stats(token, REPO_OWNER, REPO_NAME, sha))
+
 # Save the fetched commit data to a CSV file
 csvRepository.save_commits_to_csv(commits_data, './dags/commits.csv')
--- dags/gitHubClient.py
+++ dags/gitHubClient.py
@@ -38,3 +38,43 @@ def fetch_github_commits(repo_owner, repo_name, token):
     except requests.RequestException as e:
         print(f"Error fetching commits: {e}")
         return []
+
+
+def fetch_gitHub_commit_stats(token, repo_owner, repo_name, sha):
+    """
+    Fetches a full commit from a GitHub repository.
+
+    Parameters:
+        sha (str): The commit SHA.
+        repo_owner (str): The owner of the GitHub repository.
+        repo_name (str): The name of the GitHub repository.
+        token (str): Your GitHub personal access token.
+
+    Returns:
+        dict: A dictionary of an extra commit data.
+    """
+    commit_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/commits/{sha}'
+    headers = {'Authorization': f'token {token}'}
+    commit_info = {}
+
+    try:
+        commit_response = requests.get(commit_url, headers=headers)
+        commit_response.raise_for_status()  # Check for request errors
+
+        commit_stats = commit_response.json()
+
+        # Read additions and deletions
+        stats = commit_stats.get('stats', {})
+
+        commit_info = {
+            'sha': sha,
+            'additions': stats.get('additions', 0),
+            'deletions': stats.get('deletions', 0),
+            'total': stats.get('total', 0)
+        }
+
+        return commit_info
+
+    except requests.RequestException as e:
+        print(f"Error fetching commits: {e}")
+        return commit_info
