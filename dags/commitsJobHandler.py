72f9cd179a323731b6089d735026e962f91b8b16
Olga Kindeeva <olgakindeeva@gmail.com>
2025-03-24 12:14:54 -0700

    transformed data fetching from GitHub to csv to Airflow task inside a DAG, set tasks sequence

--- dags/commitsJobHandler.py
+++ dags/commitsJobHandler.py
@@ -1,30 +1,21 @@
-# GitHubInsights fetches commits data from GitHub and saves it to a CSV file.
-
-import os
-from dotenv import load_dotenv
 import csvRepository
 import gitHubClient
 
 
-# Load environment variables from .env file
-load_dotenv()
+# Fetch commits data from GitHub to commits.csv
+# REPO_OWNER - owner of the repository to fetch commits from
+# REPO_NAME - name of the repository to fetch commits from
+# token - GitHub API token for authentication
 
-# Example usage:
-# REPO_OWNER = 'okind'
-# REPO_NAME = 'algorithmic_programming'
-REPO_OWNER = 'dddat1017'
-REPO_NAME = 'Scraping-Youtube-Comments'
-token = os.getenv('GITHUB_TOKEN')
 
-# Fetch commit data from GitHub
-# Provide repo data: repo_owner, repo_name, token
-commits_data = gitHubClient.fetch_github_commits(REPO_OWNER, REPO_NAME, token)
+def extract_commits_data(REPO_OWNER, REPO_NAME, token):
+    commits_data = gitHubClient.fetch_github_commits(
+        REPO_OWNER, REPO_NAME, token)
+    for commit in commits_data:
+        sha = commit['sha']
+        # Add commit stats to commit data
+        commit.update(
+            gitHubClient.fetch_gitHub_commit_stats(token, REPO_OWNER, REPO_NAME, sha))
 
-for commit in commits_data:
-    sha = commit['sha']
-    # Add commit stats to commit data
-    commit.update(
-        gitHubClient.fetch_gitHub_commit_stats(token, REPO_OWNER, REPO_NAME, sha))
-
-# Save the fetched commit data to a CSV file
-csvRepository.save_commits_to_csv(commits_data, './dags/commits.csv')
+    # Save the fetched commit data to a CSV file
+    csvRepository.save_commits_to_csv(commits_data, './dags/commits.csv')
--- dags/commits_load.py
+++ dags/commits_load.py
@@ -4,12 +4,20 @@ from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 
 from airflow.operators.python import PythonOperator
 
+from airflow.models import Variable
+
 import datetime
 
 import pandas as pd
 
 import logging
+
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+import commitsJobHandler
+from dotenv import load_dotenv
+import os
+
 
 # Default DAG arguments
 default_args = {
@@ -26,11 +34,25 @@ with DAG(
     catchup=False
 ) as dag:
 
-    logging.basicConfig(level=logging.DEBUG)
-    # Read CSV and load to Snowflake
+     logging.basicConfig(level=logging.DEBUG)
 
+    # Fetch commits data from GitHub and save it to a CSV file commits.csv
+    def extract_github_commits(*args):
+
+        # Read the token from the airflow variable
+        github_token = Variable.get("GITHUB_TOKEN")
+
+        logging.info('GitHub token provided = %s', github_token != None)
+
+        REPO_OWNER = args[0]
+        REPO_NAME = args[1]
+        commitsJobHandler.extract_commits_data(
+            REPO_OWNER, REPO_NAME, github_token)
+
+    # Read commits.csv and load commits to Snowflake
     def load_csv_to_snowflake():
-        # Read CSV file (Assuming the CSV is available locally)
+
+        # Read CSV file into a DataFrame
         df = pd.read_csv('/opt/airflow/dags/commits.csv')
 
         # Convert DataFrame to a list of tuples
@@ -55,11 +77,17 @@ with DAG(
         cursor.close()
         conn.close()
 
-    # Task to load CSV data
+    # Task to extract data from repo 'Scraping-Youtube-Comments' in 'dddat1017' account to commits.csv
+    extract_data = PythonOperator(
+        task_id='extract_data_from_github',
+        op_args=['dddat1017', 'Scraping-Youtube-Comments', None],
+        python_callable=extract_github_commits
+    )
+    # Task to load commits data to Snowflake
     load_csv_data = PythonOperator(
         task_id='load_csv_to_snowflake',
         python_callable=load_csv_to_snowflake
     )
 
-    # Set task dependencies (only one task here)
-    load_csv_data
+    # Set task dependencies for the DAG
+    extract_data >> load_csv_data
