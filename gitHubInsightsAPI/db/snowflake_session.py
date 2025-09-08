3ac44d5c9d49f5e1dd6944402c33c05cd01a49b0
Olga Kindeeva <olgakindeeva@gmail.com>
2025-09-07 21:40:40 -0700

    Fixed snowflake session creation method so it retuns an active session

--- gitHubInsightsAPI/db/snowflake_session.py
+++ gitHubInsightsAPI/db/snowflake_session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from core.config import settings
 
 sf_engine = create_engine(settings.SNOWFLAKE_SQLALCHEMY_URL)
@@ -7,14 +8,15 @@ SnowflakeSession = sessionmaker(
     bind=sf_engine, autoflush=False, expire_on_commit=False)
 
 
-def get_snowflake_session():
+def get_snowflake_session() -> Session:
     """FastAPI dependency (sync) or general context manager."""
-    session = SnowflakeSession()
     try:
         with sf_engine.connect() as conn:
             result = conn.execute(text("SELECT CURRENT_VERSION()")).fetchone()
             print(f"Connected to Snowflake, version: {result[0]}")
+
+        session = SnowflakeSession()
     except Exception as e:
         print(f"Failed to connect to Snowflake: {e}")
-    finally:
-        session.close()
+
+    return session
